1e94d5bef4a2b92981718685070582f559fde8c3
aidiel <aidielrachman@gmail.com>
2023-03-14 11:07:51 +0700

    sensor based causality analysis: 1

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-"""Dimentional Reduction on Botnet Data"""
+"""Paralel Botnet Data Forensic"""
 """Writen By: M. Aidiel Rachman Putra"""
 """Organization: Net-Centic Computing Laboratory | Institut Teknologi Sepuluh Nopember"""
 
 import warnings
 warnings.simplefilter(action='ignore')
 import bin.interfaces.cli.main as cli
-from bin.modules.domain import *
+import bin.modules.spm4Detection.domain as spm4d
+import bin.modules.sensorBasedPattern.domain as sbp
 
 if __name__ == "__main__":
   listMenu = [
-    ('Sensor Based Causality Analysis', sensorBasedCausalityAnalysis)
+    ('Sensor Based Causality Analysis', sbp.main),
+    ('Sequence Pattern Mining for Detection', spm4d.main),
   ]
   cli.menu(listMenu)
